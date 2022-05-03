70e1081fa82df686d33b575352c7a102d5c7f282
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-05-03 20:59:36 +0530

    Added router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MockMan from "mockman-js";
 import "./App.css";
+import Home from "./pages/home/Home";
 
 
 function App() {
-  return <>hello</>
+  
+return (
+    <Routes>
+      <Route path="/mockman" element={<MockMan/>} />
+      <Route path="/" element={<Home/>} />
+    </Routes>
+);
+   
+
 }
 
 export default App;
