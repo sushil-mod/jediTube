87cf37fd437afa6650056c687b0d6edd587de625
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2022-06-15 11:15:07 +0530

    Feat: apicalls and reducer for video ,category

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authenticationReducer  from '../features/authentication/authSlice';
+import videoListReducer from '../features/video/videoSlice';
+import categoryListReducer from '../features/category/categorySlice';
 
 export const store = configureStore({
     reducer:{
         authentication : authenticationReducer,
+        videoList : videoListReducer ,
+        categoryList : categoryListReducer ,
     }
 })
